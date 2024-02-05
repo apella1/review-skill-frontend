30eb23a7bb6a0df7f80291ecfd76d5760c4707e3
apella1 <johncollons5@gmail.com>
2024-02-05 16:28:43 +0300

    feat: add user log out

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,6 +1,15 @@
-import { Avatar } from "@mui/material";
+import {
+  Avatar,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { IoMdLogOut } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { client } from "../axios/axios";
 import { DBUser, SidebarLink } from "../types/types";
 
@@ -40,12 +49,34 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const [user, setUser] = useState<DBUser>({} as DBUser);
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setSearch(value);
   };
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  const handleSettings = () => {
+    navigate("/dashboard/settings");
+  };
+
   const location = useLocation();
 
   useEffect(() => {
@@ -116,7 +147,68 @@ export default function DashboardLayout({
               className="w-[50%] border border-[#D9D9D9] py-2 px-4 text-[#000000] text-[14px] leading-[20px] rounded-[8px] placeholder:text-[#7C7C8D] placeholder:text-[14px] placeholder:leading-[20px]"
             />
           )}
-          <Avatar {...stringAvatar(`${user.first_name} ${user.last_name}`)} />
+          <Tooltip title="Account settings">
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{ ml: 2 }}
+              aria-controls={open ? "account-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+            >
+              <Avatar
+                {...stringAvatar(`${user.first_name} ${user.last_name}`)}
+              />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&::before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem onClick={handleSettings}>
+              <ListItemIcon>
+                <IoSettingsOutline />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem onClick={handleLogout}>
+              <ListItemIcon>
+                <IoMdLogOut />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
         </section>
         {children}
       </section>
