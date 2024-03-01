d52261804a90c6585f4bdc2a691dece8a4c63a29
apella1 <johncollons5@gmail.com>
2024-03-01 12:26:43 +0300

    test: assert specific heading element containing the title

--- tests/components/HomeHeader.test.tsx
+++ tests/components/HomeHeader.test.tsx
@@ -2,10 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { HomeHeader } from "../../src/components";
 
 describe("HomeHeader", () => {
-  it("should contain the application title", () => {
+  it("should contain the application title in an h1", () => {
     render(<HomeHeader />);
-    const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading", {
+      name: "ReviewSkill",
+      level: 1,
+    });
     expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent("ReviewSkill");
   });
 });
