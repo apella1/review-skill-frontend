f3c85fc240269533e9be7d939ad50b4e17d4ec72
apella1 <johncollons5@gmail.com>
2025-04-06 19:49:23 +0300

    feat: update home header copy

--- src/components/home/home-header.tsx
+++ src/components/home/home-header.tsx
@@ -10,14 +10,15 @@ export default function HomeHeader() {
       <div className="space-y-6">
         <div className="space-y-4">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-            ReviewSkill
+            Unlock your potential with <span>ReviewSkill!</span>
           </h1>
-          <div className="mx-auto max-w-[700px] space-y-2 text-muted-foreground">
+          <div className="mx-auto space-y-2 text-muted-foreground">
             <h2 className="text-lg sm:text-xl md:text-2xl">
-              Spaced Repetition Tools To Enhance Your Career
-            </h2>
-            <h2 className="text-lg sm:text-xl md:text-2xl">
-              Build Your Skills In a Science-Based Way
+              Our innovative learning platform harnesses the power of spaced
+              repetition and interactive tools to help you master new concepts
+              efficiently. Whether you&apos;re studying for exams, learning a
+              new language, or exploring a new subject, we&apos;re here to guide
+              you every step of the way.
             </h2>
           </div>
         </div>
